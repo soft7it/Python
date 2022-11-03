fe81c97becb4e1457554eaaacfff58ca4205cd95
VITALIE 777 <soft7.it@gmail.com>
2022-11-03 06:43:00 +0000

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,9 @@
+# IN : Meteo source < air temperature Celsius
+# OUT : determine if its "Warm", "Cold" 
+
+temperatureC = 20
+
+if temperatureC >= 18:
+    print( "Warm" )
+else:
+    print( "Cold" )
