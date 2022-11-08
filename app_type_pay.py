7864f38c711f7d68c83ced06f368b839cc71c912
VITALIE 777 <soft7.it@gmail.com>
2022-11-08 16:56:59 +0000

    Update app_type_pay.py

--- app_type_pay.py
+++ app_type_pay.py
@@ -13,27 +13,31 @@ service_price = 100
 # clear some :
 system("cls")
 
-client_cash_amount = 50
-client_card_amount = 150
-client_crypto_amount = 250
+# client_cash_amount = 50
+# client_card_amount = 150
+# client_crypto_amount = 250
+#check amount crypto to converting
+crypto_1_coin = 5 # 5 din 100 echivalentul valorii
+
 
 # prma intrebare
-#client_cash_amount = int(input("enter cash amount: "))
-
+client_cash_amount = int(input("enter cash amount: "))
 if client_cash_amount >= service_price:
     print("CASH Payment success!!!")
 else:
     print("CASH Payment FAILURE!!!")    
 
     # intrebarea 2 CU CARD:
-    #client_card_amount = int(input("enter card amount: "))
+    client_card_amount = int(input("enter card amount: "))
     if client_card_amount >= service_price:
         print("CARD PAYMENT SUCCESS!!!")
     else:
         print("CARD PAYMENT FAILURE!!!")    
-    # intrebarea 3 crypto:
-    #client_crypto_amount = int(input("enter crypto amount: "))
-        if client_crypto_amount >= service_price:
+        # intrebarea 3 crypto:
+        client_crypto_amount = int(input("enter crypto amount: "))
+        #mentine ierarhia operatiunilor altfel nu merge !!!
+        total_cryto = crypto_1_coin * client_crypto_amount
+        if total_cryto >= service_price:
             print("Payment crypto success!!!")
         else:
-            print("Payment crypto FAILURE!!!")    
+            print("Payment crypto FAILURE!!!")  
