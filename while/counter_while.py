7101718499ce82cb47583b6297f98e33b0ca0371
VITALIE 777 <soft7.it@gmail.com>
2022-11-10 06:53:00 +0000

    examples while + & - , & found number exact

---
+++ while/counter_while.py
@@ -0,0 +1,17 @@
+## while Counter
+
+# - 3 keypoints: start, step, end
+# - direct/reverse
+# - diagram
+# - breakpoints
+# - shortcuts
+
+start = 1
+end = 10
+n = start
+
+
+while n <= end:
+    print(n)
+
+    n += 1
---
+++ while/counter_while_minus.py
@@ -0,0 +1,21 @@
+# while Counter
+
+# - 3 keypoints: start, step, end
+# - direct/reverse
+# - diagram
+# - breakpoints
+# - shortcuts
+
+start = 10
+end = 1
+n = start
+
+x = 3
+
+while n >= end:
+    print(n)
+
+    if n == x:
+        print("Found!")
+
+    n -= 1
