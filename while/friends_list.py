b00f7152fb5ef402c9eae4cac2a0bb5348aaade3
VITALIE 777 <soft7.it@gmail.com>
2022-12-02 05:59:18 +0000

    while&add_thowin_for_in

---
+++ while/friends_list.py
@@ -0,0 +1,17 @@
+# Fact: list of friends
+
+# empty list
+
+friends = []
+
+while len( friends ) < 100:
+    name = input("Add friend name: ")
+    if name == "":
+        break
+    # HW : check if the name is in the list
+    #   operator in
+    friends.append( name )
+
+print("You have", len( friends ), "friends")
+for i in range( len(friends) ):
+    print(" ",i,">>",friends[i])    
