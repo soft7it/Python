2ee0e1e7f83511f2a3872d51e21e7120e65f91cb
VITALIE 777 <soft7.it@gmail.com>
2022-12-19 07:06:39 +0000

    test_list

---
+++ data_input_list/list.py
@@ -0,0 +1,5 @@
+# ,     "bk", "bq", "bb", "bn", "br", "bp"
+pieces_codes = [" ", "wk", "wq", "wb", "wn", "wr", "wp", ]
+# index           0,   1,    2,   3,     4,    5,    6
+for in_ in pieces_codes:
+    print(in_, pieces_codes)
