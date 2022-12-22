0d3333bdd26958cf1d0f44a46c8188c193fd9af9
VITALIE 777 <soft7.it@gmail.com>
2022-12-22 07:29:30 +0000

    update_impruve_code_test_done

--- Function/triangle_drawing.py
+++ Function/triangle_drawing.py
@@ -12,13 +12,26 @@ def triangle( h, o ) :
     s = 1
     p = h - 1 # p = 3 - 1 solutionam distanta din stinga
     for r in range(h):
+        if h == 3 :
+            o = 6
+        elif h == 5 :
+            s = 3
+            h = 6
+            o = 3
+        elif h == 7:            
+            s = 5
+            h = 4
         print( "  " * o + "  " * p + "* " * s )
         s += 2
         p -= 1
-
+        
+        
 #######################
 triangle(3, 4)        
 triangle(5, 2)        
 triangle(7, 0)   
 
-# HW : try to modify the function so it skips a few rows, use if + ignore print 
+# HW : try to modify the function so it skips a few rows,
+#  use if + ignore print 
+
+#### Test done ######
