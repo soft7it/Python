60cc2cb370d7d1369159b1d0a281db2e2b748235
VITALIE 777 <soft7.it@gmail.com>
2022-12-26 06:04:21 +0000

    function_sample

---
+++ Function/f_call_sum.py
@@ -0,0 +1,10 @@
+# Create & call a function that sums 3 integers - reutrns the result
+
+def sum( a, b, c):
+    # local variable
+    s = a + b + c
+    return s
+
+# global variable
+res = sum( 1, 2, 3 )
+print(res)    
