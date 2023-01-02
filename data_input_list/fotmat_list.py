f45d6dccbd34c3d5d7cdce1493921d5f2e3dc3fd
VITALIE 777 <soft7.it@gmail.com>
2023-01-02 00:08:35 +0000

    formaring_list

---
+++ data_input_list/fotmat_list.py
@@ -0,0 +1,13 @@
+data = {
+    "name": "John Doe",
+    "age": 30,
+    "averege_salary": 11000.50
+}
+
+formats = {
+    "{value:>20}",
+    "{value:>3}",
+    "{value:>10.2f}",
+}
+
+# print(f"{data['name']:>20} {data['age']:>3} {data['averege_salary']:>10.2f}")
