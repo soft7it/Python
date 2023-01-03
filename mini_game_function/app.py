1b797a1c08c9d39979d2d407307a57015e04313b
VITALIE 777 <soft7.it@gmail.com>
2023-01-03 08:41:58 +0000

    cosmos_0_9

--- mini_game_function/app.py
+++ mini_game_function/app.py
@@ -1,18 +1,22 @@
 from map import *
 from ui import *
+from radar import *
 
-while True:
+while True: #rc != 9 or rc != 0 or rr != 9 or rr != 0:  #
     clear()
     printMap( gameMap )
     key = controls()
+    
 
-    if key == 'x':  # iese din jok
+    if key == 'x':  # tasta iese din jok
         break
 
     gameMap[rr][rc] = 0 # erase the robot
     
     if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
-        if rc != 9:            
+        if rc == 9:
+            break
+        else:
             rc += 1             # increment coord
             gameMap[rr][rc] = 2
 
@@ -26,8 +30,10 @@ while True:
     # : map add same dange mark X, in the moment you move you have radar position rc+1, rc+2 not mine   
     # in casa od danger -> WARNING -> danger detection
     if key == "s" and gameMap[rr+1][rc] != 1: # directia spre jos
-        rr += 1
-        gameMap[rr][rc] = 2
+        if rr != 9:
+            rr += 1
+            gameMap[rr][rc] = 2
     if key == "w" and gameMap[rr-1][rc] != 1: # directia spre sus
-        rr -=1
-        gameMap[rr][rc] = 2
+        if rr != 0:
+            rr -=1
+            gameMap[rr][rc] = 2
