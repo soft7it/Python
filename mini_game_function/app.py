30f11891c401cb1fb0d949ff4e0e2397ac0686a7
VITALIE 777 <soft7.it@gmail.com>
2023-01-04 07:54:17 +0000

    renwe_but_error

--- mini_game_function/app.py
+++ mini_game_function/app.py
@@ -1,8 +1,9 @@
 from map import *
 from ui import *
-from radar import *
+# from os import rmdir
+# rmdir("C:\Users\soft7\OneDrive\Documents\Python_lesson\Test_python_HW\mini_game_function\__pycache__")
 
-while True: #rc != 9 or rc != 0 or rr != 9 or rr != 0:  #
+while True: 
     clear()
     printMap( gameMap )
     key = controls()
@@ -10,30 +11,32 @@ while True: #rc != 9 or rc != 0 or rr != 9 or rr != 0:  #
 
     if key == 'x':  # tasta iese din jok
         break
-
-    gameMap[rr][rc] = 0 # erase the robot
-    
-    if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
-        if rc == 9:
-            break
-        else:
+    if rc < 9:
+        
+        gameMap[rr][rc] = 0 # erase the robot
+        
+        if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
+            
             rc += 1             # increment coord
             gameMap[rr][rc] = 2
-
-    if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
-        if rc != 0:
+    if 0 > rc :
+        
+        if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
+            
             rc -= 1             # decrement coord
             gameMap[rr][rc] = 2  # punem robotul la lok
 
-    # HW: add Up/Down movement 
-    # : bondaries check right -> 9, left - 0 stop
-    # : map add same dange mark X, in the moment you move you have radar position rc+1, rc+2 not mine   
-    # in casa od danger -> WARNING -> danger detection
-    if key == "s" and gameMap[rr+1][rc] != 1: # directia spre jos
-        if rr != 9:
+        # HW: add Up/Down movement 
+        # : bondaries check right -> 9, left - 0 stop
+        # : map add same dange mark X, in the moment you move you have radar position rc+1, rc+2 not mine   
+        # in casa od danger -> WARNING -> danger detection
+    if rr < 9:    
+        if key == "s" and gameMap[rr+1][rc] != 1: # directia spre jos
+            
             rr += 1
             gameMap[rr][rc] = 2
-    if key == "w" and gameMap[rr-1][rc] != 1: # directia spre sus
-        if rr != 0:
+    if 0 > rr:        
+        if key == "w" and gameMap[rr-1][rc] != 1: # directia spre sus
+            
             rr -=1
             gameMap[rr][rc] = 2
