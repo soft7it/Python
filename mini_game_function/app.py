83fe8d06cdff337c86a9b6c54355beb6e2943655
VITALIE 777 <soft7.it@gmail.com>
2023-01-04 08:31:26 +0000

    update_error

--- mini_game_function/app.py
+++ mini_game_function/app.py
@@ -11,32 +11,32 @@ while True:
 
     if key == 'x':  # tasta iese din jok
         break
-    if rc < 9:
+    
         
-        gameMap[rr][rc] = 0 # erase the robot
-        
-        if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
-            
+    gameMap[rr][rc] = 0 # erase the robot
+    
+    if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
+        if rc < 9:
             rc += 1             # increment coord
             gameMap[rr][rc] = 2
-    if 0 < rc :
-        
-        if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
-            
+
+    
+    if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
+        if 0 < rc :
             rc -= 1             # decrement coord
             gameMap[rr][rc] = 2  # punem robotul la lok
 
-        # HW: add Up/Down movement 
-        # : bondaries check right -> 9, left - 0 stop
-        # : map add same dange mark X, in the moment you move you have radar position rc+1, rc+2 not mine   
-        # in casa od danger -> WARNING -> danger detection
-    if rr < 9:    
-        if key == "s" and gameMap[rr+1][rc] != 1: # directia spre jos
-            
+    # HW: add Up/Down movement 
+    # : bondaries check right -> 9, left - 0 stop
+    # : map add same dange mark X, in the moment you move you have radar position rc+1, rc+2 not mine   
+    # in casa od danger -> WARNING -> danger detection
+    
+    if key == "s" and gameMap[rr+1][rc] != 1: # directia spre jos
+        if rr < 9:
             rr += 1
             gameMap[rr][rc] = 2
-    if 0 < rr:        
-        if key == "w" and gameMap[rr-1][rc] != 1: # directia spre sus
-            
+        
+    if key == "w" and gameMap[rr-1][rc] != 1: # directia spre sus
+        if 0 < rr:
             rr -=1
             gameMap[rr][rc] = 2
