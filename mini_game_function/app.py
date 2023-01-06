d3d40ff072120c6eb7752ba3c1b5e87967e0cfc5
VITALIE 777 <soft7.it@gmail.com>
2023-01-06 16:37:41 +0000

    test

--- mini_game_function/app.py
+++ mini_game_function/app.py
@@ -7,26 +7,50 @@ while True:
     clear()
     printMap( gameMap )
     key = controls()
-    
-
+    ####################################################################################
     if key == 'x':  # tasta iese din jok
-        break        
-    
-    if rc <= 8:       
-        if key == "d" and gameMap[rr][rc+1] != 3 or key == "d" and gameMap[rr][rc+2] != 3: # :
-            print("WARNING DANGER!!!")
-            input()
-            if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
-                    
-                gameMap[rr][rc] = 0 # erase the robot
-                rc += 1             # increment coord
-                gameMap[rr][rc] = 2
+        break
 
-    if 0 < rc :
-        if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
+    
+        # :
+        
+    # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1       
+    
+    if key == "d" and rc <= 8:
+        if key == "d" and gameMap[rr][rc+1] != 1:
+             
             gameMap[rr][rc] = 0  # erase the robot
-            rc -= 1             # decrement coord
-            gameMap[rr][rc] = 2  # punem robotul la lok
+            rc += 1             # increment coord
+            gameMap[rr][rc] = 2 
+                # if key == "d" and gameMap[rr][rc+2] != 3 or key == "d" and gameMap[rr][rc+1] != 3:
+                    # print("WARNING DANGER!!!")            
+                 
+# if 0 < rc:
+    #     if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
+    #         gameMap[rr][rc] = 0  # erase the robot
+    #         rc -= 1             # decrement coord
+    #         gameMap[rr][rc] = 2  # punem robotul la lok
+        ##################################################################################
+    # if key == 'x':  # tasta iese din jok
+    #     break        
+    
+    # if rc <= 8:       
+    #     if key == "d" and gameMap[rr][rc+1] != 3 or key == "d" and gameMap[rr][rc+2] != 3: # :
+    #         print("WARNING DANGER!!!")
+    #         input()
+    #         if key == "d" and gameMap[rr][rc+1] != 1: # directia spre dreapta cu conditia daca se misca cu +1 nu intilneste 1
+                    
+    #             gameMap[rr][rc] = 0 # erase the robot
+    #             rc += 1             # increment coord
+    #             gameMap[rr][rc] = 2
+
+    # if 0 < rc :
+    #     if key == "a" and gameMap[rr][rc-1] != 1:  # directia spre stinga
+    #         gameMap[rr][rc] = 0  # erase the robot
+    #         rc -= 1             # decrement coord
+    #         gameMap[rr][rc] = 2  # punem robotul la lok
+
+            ##############################################################
 
     # HW: add Up/Down movement 
     # : bondaries check right -> 9, left - 0 stop
